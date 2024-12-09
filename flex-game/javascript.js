bf90c636fa07cc7414ec9cd881144c28a8bf6ebe
Gabriel Angelo <gabrielangelolira@gmail.com>
2024-12-09 14:32:58 -0300

    first commit

---
+++ flex-game/javascript.js
@@ -0,0 +1,13 @@
+const arena = document.querySelector('.arena');
+
+function setDirection(direction) {
+    arena.style.flexDirection = direction;
+}
+
+function setJustifyContent(justify) {
+    arena.style.justifyContent = justify;
+}
+
+function setAlignItems(align) {
+    arena.style.alignItems = align;
+}
